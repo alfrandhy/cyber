f4939c1b9fe7f3dcadfad9e35f0533d1653de455
Piksi Megatama <alfrandhy@gmail.com>
2020-03-18 16:37:58 +0700

    add home

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -4,7 +4,21 @@ class Home extends BaseController
 {
 	public function index()
 	{
-		return view('welcome_message');
+		$data = array(	
+					'title'			=> 'Cyber App - Base',
+					'main_title'	=> 'Cyber App',
+					'content'		=> 'home/homeindex',
+				);
+		return view('layouts/base',$data);
+	}
+	public function about()
+	{
+		$data = array(	
+					'title'			=> 'Cyber App - Base',
+					'main_title'	=> 'Cyber App',
+					'content'		=> 'home/about',
+				);
+		return view('layouts/base',$data);
 	}
 
 	//--------------------------------------------------------------------
---
+++ app/Views/layouts/head.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title><?php echo $title ?></title>
+  <!-- Favicons -->
+  <link href="<?php echo base_url(); ?>/assets/img/favicon.png" rel="icon">
+  <link href="<?php echo base_url(); ?>/assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Roboto:300,300i,400,400i,500,500i,700,700i&display=swap" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="<?php echo base_url(); ?>/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="<?php echo base_url(); ?>/assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+  <link href="<?php echo base_url(); ?>/assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+  <link href="<?php echo base_url(); ?>/assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="<?php echo base_url(); ?>/assets/vendor/venobox/venobox.css" rel="stylesheet">
+  <link href="<?php echo base_url(); ?>/assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="<?php echo base_url(); ?>/assets/vendor/aos/aos.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="<?php echo base_url(); ?>/assets/css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: Moderna - v2.0.1
+  * Template URL: https://bootstrapmade.com/free-bootstrap-template-corporate-moderna/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+
+</head>
+<body>
